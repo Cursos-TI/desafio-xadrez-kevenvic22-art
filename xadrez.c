bd990bd1fa5667f47b97680f928b241faed033a3
Keven Victor Dos Santos <kevenvic22@gmail.com>
2025-10-28 01:34:08 +0000

    Desafio aventureiro concluído com sucesso!

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,7 @@ int main() {
     int movimento_bispo = 1; // O Bispo mve-se na diagona
     int movimento_torre = 1;// A Torre,move-se em linha reta horizontalmente ou verticalmente
     int movimento_rainha = 1; // A Rainha move-se em qualquer direção (horizontal, vertical, diagonal)
+    int movimento_cavalo = 1; // O Cavalo move-se em "L", duas casas em uma direção e depois uma casa perpendicularmente
     //Movimentação do Bispo
     printf("Movimentação do Bispo:\n");
     while (movimento_bispo <= 5)
@@ -26,9 +27,17 @@ int main() {
         printf("Diagonal Esquerda\n");
     }
    
-    // Nível Aventureiro - Movimentação do Cavalo
-    
-
+    // Movimentação do Cavalo
+    printf("Movimentação do Cavalo:\n");
+    while (movimento_cavalo--)
+    {
+      for ( int i = 0; i < 2; i++)
+      {
+        printf("Baixo\n");
+      }
+      printf("Esquerda\n");
+    }
+  
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
